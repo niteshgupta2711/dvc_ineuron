482fc4b43ff85236a28e4833d17d5e50fae83dea
Nitesh kumar Gupta <guptanitesh2711@gmail.com>
2022-09-20 20:36:56 +0530

    tess addded

--- template.py
+++ template.py
@@ -3,7 +3,12 @@ from pathlib import Path
 package_name='DEEP_Classifier'
 
 list_of_files=['.github/workflows/.gitkeep',
-f'src/{package_name}/__init__.py',
+f'src/{package_name}/components/__init__.py',
+f'src/{package_name}/pipeline/__init__.py',
+f'src/{package_name}/entity/__init__.py',
+f'src/{package_name}/utils/__init__.py',
+f'src/{package_name}/utils/common.py',
+f'src/{package_name}/constants/__init__.py',
 'tests/__init__.py',
 'tests/unit/__init__.py', # testing a single component
 'tests/integration/__init__.py', # testing multiple components
